19e24170ad5a4510f4ba95053c112a03077d486f
조성재 <chungwoohyuk@jeong-uhyeog-ui-MacBookAir.local>
2023-12-23 00:18:31 +0900

    first

---
+++ app.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+const passport = require('passport');
+const helmet = require('helmet');
+const hpp = require('hpp');
+const logger = require('./logger')
+const redis = require('redis');
+const RedisStore = require('connect-redis')(session);
+
+dotenv.config();
+
+const redisClient = redis.createClient({
+    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    password: process.env.REDIS_PASSWORD,
+    legacyMode: true,
+  });
+redisClient.connect().catch(console.error);
+
+const pageRouter = require('./routes/page');
+const authRouter = require('./routes/auth');
+const postRouter = require('./routes/post');
+const userRouter = require('./routes/user');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
+
+
+const app = express();
+passportConfig(); // 패스포트 설정
+
+app.set('port', process.env.PORT || 8001);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+sequelize.sync()
+    .then(()=>{
+        logger.log('db연결')
+    })
+    .catch((err)=>{
+        logger.log(err);
+    })
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+const sessionOption = {
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    store: new RedisStore({client:redisClient})
+  };
+  
+if (process.env.NODE_ENV === 'production') {
+    app.enable('trust proxy');
+    app.use(morgan('combined'));
+    app.use(helmet({      
+        contentSecurityPolicy: false,
+        crossOriginEmbedderPolicy: false,
+        crossOriginResourcePolicy: false,
+     }));
+    app.use(hpp());
+    sessionOption.proxy = true;
+    // sessionOption.cookie.secure = true;
+  }else{
+    app.use(morgan('dev'));
+  }
+
+app.use(session(sessionOption));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/', pageRouter);
+app.use('/auth', authRouter);
+app.use('/post', postRouter);
+app.use('/user', userRouter);
+// 라우터로 가고 없으면 아래 에러 처리 
+
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+//배포모드 아닐 때만 에러 나오게 ㅇㅇ
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
